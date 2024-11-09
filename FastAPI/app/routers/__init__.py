c786b3fa1de13a93958c851f494b58ac75d55359
YoungkiJo <83441265+YoungkiJo@users.noreply.github.com>
2024-11-09 14:13:40 +0900

    Delete FastAPI directory

--- FastAPI/app/routers/__init__.py
+++
@@ -1,6 +0,0 @@
-from configs.config import setting
-
-from concurrent.futures import ThreadPoolExecutor
-
-threads = dict()
-executor = ThreadPoolExecutor(max_workers=setting().thread)
--- FastAPI/app/schemas/schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class ui2simul(BaseModel):
-    masterid: str
-
-class ui2simul_confirm(BaseModel):
-    masterid: str
